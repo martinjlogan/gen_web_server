33b5f9ee88ac27ed836691c5a81786a057cfd47b
Martin Logan <martinjlogan@Macintosh.local>
2010-04-27 22:18:15 -0500

    fixed a couple small issues

--- lib/gen_web_server/src/gws_server.erl
+++ lib/gen_web_server/src/gws_server.erl
@@ -8,16 +8,8 @@
 %%%-------------------------------------------------------------------
 -module(gws_server).
 
--behaviour(gen_server).
-
 %% API
 -export([start_link/3]).
-
-%% gen_server callbacks
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-	 terminate/2, code_change/3]).
-
--define(SERVER, ?MODULE). 
 
 -record(state, {lsock, socket, request_line, headers = [], body = <<>>,
 		content_remaining = 0, callback, user_state, parent}).
@@ -34,140 +26,75 @@
 %% @end
 %%--------------------------------------------------------------------
 start_link(Callback, LSock, UserArgs) ->
-    gen_server:start_link(?MODULE, [Callback, LSock, UserArgs, self()], []).
-
-%%%===================================================================
-%%% gen_server callbacks
-%%%===================================================================
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Initiates the server
-%%
-%% @spec init(Args) -> {ok, State} |
-%%                     {ok, State, Timeout} |
-%%                     ignore |
-%%                     {stop, Reason}
-%% @end
-%%--------------------------------------------------------------------
-init([Callback, LSock, UserArgs, Parent]) ->
-    error_logger:info_msg("in init calling to ~p~n", [Callback]),
-    {ok, UserState} = Callback:init(UserArgs),
-    {ok, #state{lsock = LSock, callback = Callback, user_state = UserState, parent = Parent}, 0}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling call messages
-%%
-%% @spec handle_call(Request, From, State) ->
-%%                                   {reply, Reply, State} |
-%%                                   {reply, Reply, State, Timeout} |
-%%                                   {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, Reply, State} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
-handle_call(_Request, _From, State) ->
-    Reply = ok,
-    {reply, Reply, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling cast messages
-%%
-%% @spec handle_cast(Msg, State) -> {noreply, State} |
-%%                                  {noreply, State, Timeout} |
-%%                                  {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
-handle_cast(_Request, State) ->
-    {noreply, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling all non call/cast messages
-%%
-%% @spec handle_info(Info, State) -> {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
-handle_info({http, _Socket, {http_header, _Length, <<"Expect">>, _, <<"100-continue">>}}, #state{headers = Headers} = State) ->
-    gen_tcp:send(State#state.socket, gen_web_server:http_reply(100)),
-    inet:setopts(State#state.socket, [{active,once}]),
-    {noreply, State#state{headers = [{'Expect', <<"100-continue">>}|Headers]}};
-handle_info({http, _Socket, {http_header, _Length, 'Content-Length', _, Value}}, #state{headers = Headers} = State) ->
-    ContentRemaining = list_to_integer(binary_to_list(Value)),
-    inet:setopts(State#state.socket, [{active,once}]),
-    {noreply, State#state{headers = [{'Content-Length', Value}|Headers], content_remaining = ContentRemaining}};
-handle_info({http, _Socket, {http_header, _Length, Key, _, Value}}, #state{headers = Headers} = State) ->
-    inet:setopts(State#state.socket, [{active,once}]),
-    {noreply, State#state{headers = [{Key, Value}|Headers]}};
-handle_info({http, _Socket, {http_request, _Method, _Path, _HTTPVersion} = RequestLine}, State) ->
-    inet:setopts(State#state.socket, [{active,once}]),
-    {noreply, State#state{request_line = RequestLine}};
-handle_info({http, _Socket, http_eoh}, #state{content_remaining = 0} = State) ->
-    Reply = callback(State),
-    gen_tcp:send(State#state.socket, Reply),
-    {stop, normal, State};
-handle_info({http, _Socket, http_eoh}, State) ->
-    inet:setopts(State#state.socket, [{active,once}, {packet, raw}]),
-    {noreply, State};
-handle_info({tcp, _Socket, Packet}, State) ->
-    PacketSize       = byte_size(Packet),
-    ContentRemaining = State#state.content_remaining - PacketSize,
-    Body             = list_to_binary([State#state.body, Packet]),
-    NewState = State#state{body = Body, content_remaining = ContentRemaining},
-    case ContentRemaining of
-	0 ->
-	    Reply = callback(NewState),
-	    gen_tcp:send(State#state.socket, Reply),
-	    {stop, normal, State};
-	ContentLeftOver when ContentLeftOver > 0 ->
-	    inet:setopts(State#state.socket, [{active,once}]),
-	    {noreply, NewState}
-    end;
-handle_info({tcp_closed, _Socket}, State) ->
-    {stop, normal, State};
-handle_info(timeout, #state{lsock = LSock, parent = Parent} = State) ->
-    {ok, Socket} = gen_tcp:accept(LSock),
-    gws_connection_sup:start_child(Parent),
-    inet:setopts(Socket,[{active,once}]),
-    {noreply, State#state{socket = Socket}}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_server terminates
-%% with Reason. The return value is ignored.
-%%
-%% @spec terminate(Reason, State) -> void()
-%% @end
-%%--------------------------------------------------------------------
-terminate(_Reason, _State) ->
-    ok.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Convert process state when code is changed
-%%
-%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
-%% @end
-%%--------------------------------------------------------------------
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
+    % Not using proc lib for efficiency concerns
+    Self = self(),
+    {ok, spawn_link(fun() -> init(Callback, LSock, UserArgs, Self) end)}.
 
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+
+init(Callback, LSock, UserArgs, Parent) ->
+    {ok, UserState} = Callback:init(UserArgs),
+    accept(#state{lsock = LSock, callback = Callback, user_state = UserState, parent = Parent}).
+
+accept(#state{lsock = LSock, parent = Parent} = State) ->
+    {ok, Socket} = gen_tcp:accept(LSock),
+    gws_connection_sup:start_child(Parent),
+    inet:setopts(Socket,[{active,once}]),
+    collect_request_line(State#state{socket = Socket}).
+
+collect_request_line(State) ->
+    receive
+	{http, _Socket, {http_request, _Method, _Path, _HTTPVersion} = RequestLine} ->
+	    inet:setopts(State#state.socket, [{active,once}]),
+	    collect_headers(State#state{request_line = RequestLine});
+	{tcp_closed, _Socket} ->
+	    ok
+    end.
+
+collect_headers(#state{headers = Headers} = State) ->
+    receive
+	{http, _Socket, {http_header, _Length, <<"Expect">>, _, <<"100-continue">>}} ->
+	    gen_tcp:send(State#state.socket, gen_web_server:http_reply(100)),
+	    inet:setopts(State#state.socket, [{active,once}]),
+	    collect_headers(State#state{headers = [{'Expect', <<"100-continue">>}|Headers]});
+	{http, _Socket, {http_header, _Length, 'Content-Length', _, Value}} ->
+	    ContentRemaining = list_to_integer(binary_to_list(Value)),
+	    inet:setopts(State#state.socket, [{active,once}]),
+	    collect_headers(State#state{headers = [{'Content-Length', Value}|Headers], content_remaining = ContentRemaining});
+	{http, _Socket, {http_header, _Length, Key, _, Value}} ->
+	    inet:setopts(State#state.socket, [{active,once}]),
+	    collect_headers(State#state{headers = [{Key, Value}|Headers]});
+	{http, _Socket, http_eoh} when State#state.content_remaining == 0 ->
+	    Reply = callback(State),
+	    gen_tcp:send(State#state.socket, Reply);
+	{http, _Socket, http_eoh} ->
+	    inet:setopts(State#state.socket, [{active,once}, {packet, raw}]),
+	    collect_body(State);
+	{tcp_closed, _Socket} ->
+	    ok
+    end.
+
+collect_body(State) ->
+    receive
+	{tcp, _Socket, Packet} ->
+	    PacketSize       = byte_size(Packet),
+	    ContentRemaining = State#state.content_remaining - PacketSize,
+	    Body             = list_to_binary([State#state.body, Packet]),
+	    NewState = State#state{body = Body, content_remaining = ContentRemaining},
+	    case ContentRemaining of
+		0 ->
+		    Reply = callback(NewState),
+		    gen_tcp:send(State#state.socket, Reply);
+		ContentLeftOver when ContentLeftOver > 0 ->
+		    inet:setopts(State#state.socket, [{active,once}]),
+		    collect_body(NewState)
+	    end;
+	{tcp_closed, _Socket} ->
+	    ok
+    end.
+
 callback(State) -> 
     #state{callback     = Callback,
 	   request_line = RequestLine,
